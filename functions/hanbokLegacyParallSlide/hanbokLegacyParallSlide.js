8662da62f8de242a0d97e886910f3bacb2cd2575
yosub <dhdytjq2@naver.com>
2022-06-30 17:53:41 +0900

    한복 뉴웨이브 colorSection Done

--- functions/hanbokLegacyParallSlide/hanbokLegacyParallSlide.js
+++ functions/hanbokLegacyParallSlide/hanbokLegacyParallSlide.js
@@ -1,22 +1,20 @@
-
 const container = document.getElementById('container');
 console.log(container);
 
 let i = 0;
 let isStatus = true;
 
-container.addEventListener('wheel', (e)=>{
-  e.preventDefault();
 
-  if(e.deltaY > 0){
+
+container.addEventListener('wheel', (e) => {
+  e.preventDefault();
+  if (e.deltaY > 0) {
     legacyBgSlide();
     isStatus = true;
-
     container.style.transform = `translateX(${-i}px)`
     // console.log(i);
-    }
-
-  if(e.deltaY < 0){
+  }
+  if (e.deltaY < 0) {
     legacyBgSlideOpposite();
     isStatus = false;
 
@@ -25,26 +23,27 @@ container.addEventListener('wheel', (e)=>{
   }
 });
 
-function legacyBgSlide(){
+function legacyBgSlide() {
   requestAnimationFrame(legacyBgSlide);
-  if(i < 800 && isStatus === true){
+  if (i < 800 && isStatus === true) {
     i++;
     console.log(i);
     container.style.transform = `translateX(${-i}px)`;
-    if(i >= 800){
+    if (i >= 800) {
       i = 800
     }
-  } 
+  }
 };
-function legacyBgSlideOpposite(){
+
+function legacyBgSlideOpposite() {
   requestAnimationFrame(legacyBgSlideOpposite);
-    i--;
-    console.log(i);
-    container.style.transform = `translateX(${-i}px)`;
-    if(i <= 0){
-      i = 0;
-    }
-  } ;
+  i--;
+  console.log(i);
+  container.style.transform = `translateX(${-i}px)`;
+  if (i <= 0) {
+    i = 0;
+  }
+};
 
 
 // legacyBgSlide();
